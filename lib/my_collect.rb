5590e9989fb556fa74a52beaa38834bd28653c64
generous-vector-9581 <ojappah@gmail.com>
2019-09-23 23:22:58 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
 
 
